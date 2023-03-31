1f5f04b2b3f4f5d4057db9b83b43438ff1ba0731
Trevor Dolby <trevor.dolby@ibm.com>
2023-03-31 09:42:56 -0500

    Add build script
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

---
+++ build-and-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# This script automates the building and testing of the shared library.
+#
+# Copyright (c) 2023 Open Technologies for Integration
+# Licensed under the MIT license (see LICENSE for details)
+#
+
+# Exit on any failure
+set -e
+
+# Move submodule projects to the correct level to be picked up by ibmint
+find ace* -name ".project" -exec dirname {} ";" | xargs -n1 -i{} echo mv {} .  | grep -v Test | grep -v Scaffold > /tmp/move-projects.sh || /bin/true
+bash /tmp/move-projects.sh
+
+# Create the work directory
+rm -rf /tmp/ace-submodule-subflowlib-level1-work-dir
+mqsicreateworkdir /tmp/ace-submodule-subflowlib-level1-work-dir
+
+
+ibmint deploy --input-path . --output-work-directory /tmp/ace-submodule-subflowlib-level1-work-dir --project SubflowLibLevel1 --project SubflowLibLevel1_ContractTest --project SubflowLibLevel1_ScaffoldApp --project SubflowLibLevel1_UnitTest --project JavaLibLevel1
+
+# ibmint optimize server new for v12.0.4 - speed up test runs
+ibmint optimize server --work-directory /tmp/ace-submodule-subflowlib-level1-work-dir --enable JVM --disable NodeJS
+
+# Run the server to run the unit tests
+IntegrationServer -w /tmp/ace-submodule-subflowlib-level1-work-dir --test-project SubflowLibLevel1_UnitTest
+
+# Run the server to run the contract tests
+IntegrationServer -w /tmp/ace-submodule-subflowlib-level1-work-dir --test-project SubflowLibLevel1_ContractTest
